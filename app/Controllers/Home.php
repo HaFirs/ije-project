1ecce600c66eb01316903194f48c9043e4704e12
HaFirs <50348831+HaFirs@users.noreply.github.com>
2021-11-03 18:51:14 +0700

    home test

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,17 @@
 
 namespace App\Controllers;
 
+use PhpParser\Node\Stmt\Echo_;
+
 class Home extends BaseController
 {
-    public function index()
+    public function index($page = 'home')
     {
-        return view('welcome_message');
+        
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        echo view('ije/templates/header', $data);
+        echo view('ije/home');
+        echo view('ije/templates/footer');
     }
 }
