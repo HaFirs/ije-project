7912867a69656aacd8fa73c9eb3454afeb9eeb62
HaFirs <50348831+HaFirs@users.noreply.github.com>
2021-11-08 19:53:22 +0700

    fix ui
    navbar & color

--- app/Views/ije/templates/header.php
+++ app/Views/ije/templates/header.php
@@ -22,7 +22,7 @@
 <body>
 	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 		<div class="container">
-			<a class="navbar-brand" href="index.html"><img alt="icon" src="assets/ije/logos/1group.png"></a>
+			<a class="navbar-brand" href="#homesec"><img alt="icon" src="assets/ije/logos/1group.png" style="height: 30px; margin-bottom: 15px;"> I<span>J</span>E</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="oi oi-menu"></span> Menu
 			</button>
@@ -33,7 +33,7 @@
 					<li class="nav-item"><a href="#newsec" class="nav-link">News</a></li>
 					<li class="nav-item"><a href="#companiesec" class="nav-link">Companies</a></li>
 					<li class="nav-item"><a href="#aboutsec" class="nav-link">About</a></li>
-					<li class="nav-item"><a href="#contactsec" class="nav-link">Contact Us</a></li>
+					<li class="nav-item"><a href="#contactsec" class="nav-link">Contact</a></li>
 				</ul>
 			</div>
 		</div>
